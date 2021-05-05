e9a807b259fa5550e877e421188b67f649bd5960
Howyoung <howyoung3@gmail.com>
2021-05-05 14:15:30 +0800

    Add log to WebRTC outbound

--- outbound/webrtc.go
+++ outbound/webrtc.go
@@ -7,6 +7,7 @@ import (
 	"github.com/howyoungzhou/golive/server"
 	"github.com/mitchellh/mapstructure"
 	"github.com/pion/webrtc/v3"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
@@ -27,11 +28,13 @@ type WebRTCOutboundOptions struct {
 type WebRTCOutbound struct {
 	options *WebRTCOutboundOptions
 	tracks  []*webrtc.TrackLocalStaticRTP
+	logger  *log.Entry
 }
 
 func NewWebRTCOutbound(options *WebRTCOutboundOptions) (*WebRTCOutbound, error) {
 	res := &WebRTCOutbound{
 		options: options,
+		logger:  log.New().WithFields(log.Fields{"module": "WebRTCOutbound"}),
 	}
 	for _, t := range options.Tracks {
 		track, err := webrtc.NewTrackLocalStaticRTP(t.CodecCapability, t.ID, t.StreamID)
@@ -58,73 +61,84 @@ func RegisterWebRTC(server *server.Server, id string, options map[string]interfa
 	return res, nil
 }
 
+func (o *WebRTCOutbound) handleSDPRequest(c *gin.Context) {
+	offer := webrtc.SessionDescription{}
+	err := c.Bind(&offer)
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		o.logger.WithField("addr", c.Request.RemoteAddr).Info("malformed SDP")
+		return
+	}
+	peerConnectionConfig := o.options.WebRTC
+	// Create a new PeerConnection
+	peerConnection, err := webrtc.NewPeerConnection(peerConnectionConfig)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		o.logger.WithField("addr", c.Request.RemoteAddr).Error("failed to create peer connection")
+		return
+	}
+
+	for _, track := range o.tracks {
+		rtpSender, err := peerConnection.AddTrack(track)
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			o.logger.WithField("addr", c.Request.RemoteAddr).Info("failed to add track")
+			return
+		}
+
+		// Read incoming RTCP packets
+		// Before these packets are returned they are processed by interceptors. For things
+		// like NACK this needs to be called.
+		go func() {
+			rtcpBuf := make([]byte, 1500)
+			for {
+				if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
+					return
+				}
+			}
+		}()
+	}
+	// Set the remote SessionDescription
+	err = peerConnection.SetRemoteDescription(offer)
+	if err != nil {
+		c.AbortWithError(http.StatusBadRequest, err)
+		o.logger.WithField("addr", c.Request.RemoteAddr).Info("failed to set remote SDP")
+		return
+	}
+
+	// Create answer
+	answer, err := peerConnection.CreateAnswer(nil)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		o.logger.WithField("addr", c.Request.RemoteAddr).Info("failed to create answer SDP")
+		return
+	}
+
+	// Create channel that is blocked until ICE Gathering is complete
+	gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
+
+	// Sets the LocalDescription, and starts our UDP listeners
+	err = peerConnection.SetLocalDescription(answer)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		o.logger.WithField("addr", c.Request.RemoteAddr).Info("failed to set local SDP")
+		return
+	}
+
+	<-gatherComplete
+
+	// Get the LocalDescription and take it to base64 so we can paste in browser
+	c.JSON(http.StatusOK, peerConnection.LocalDescription())
+	o.logger.WithField("addr", c.Request.RemoteAddr).Info("connection established")
+}
+
 func (o *WebRTCOutbound) serveHTTP() {
 	r := gin.Default()
 	r.Use(cors.New(o.options.SDPServer.CORS))
-	r.POST(o.options.SDPServer.RootPath, func(c *gin.Context) {
-		offer := webrtc.SessionDescription{}
-		err := c.Bind(&offer)
-		if err != nil {
-			c.AbortWithError(http.StatusBadRequest, err)
-			return
-		}
-		peerConnectionConfig := o.options.WebRTC
-		// Create a new PeerConnection
-		peerConnection, err := webrtc.NewPeerConnection(peerConnectionConfig)
-		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		for _, track := range o.tracks {
-			rtpSender, err := peerConnection.AddTrack(track)
-			if err != nil {
-				c.AbortWithError(http.StatusInternalServerError, err)
-				return
-			}
-
-			// Read incoming RTCP packets
-			// Before these packets are returned they are processed by interceptors. For things
-			// like NACK this needs to be called.
-			go func() {
-				rtcpBuf := make([]byte, 1500)
-				for {
-					if _, _, rtcpErr := rtpSender.Read(rtcpBuf); rtcpErr != nil {
-						return
-					}
-				}
-			}()
-		}
-		// Set the remote SessionDescription
-		err = peerConnection.SetRemoteDescription(offer)
-		if err != nil {
-			c.AbortWithError(http.StatusBadRequest, err)
-			return
-		}
-
-		// Create answer
-		answer, err := peerConnection.CreateAnswer(nil)
-		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		// Create channel that is blocked until ICE Gathering is complete
-		gatherComplete := webrtc.GatheringCompletePromise(peerConnection)
-
-		// Sets the LocalDescription, and starts our UDP listeners
-		err = peerConnection.SetLocalDescription(answer)
-		if err != nil {
-			c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		<-gatherComplete
-
-		// Get the LocalDescription and take it to base64 so we can paste in browser
-		c.JSON(http.StatusOK, peerConnection.LocalDescription())
-	})
-	r.Run(o.options.SDPServer.ListenAddress)
+	r.POST(o.options.SDPServer.RootPath, o.handleSDPRequest)
+	o.logger.WithField("addr", o.options.SDPServer.ListenAddress).Info("SDP server is listening")
+	err := r.Run(o.options.SDPServer.ListenAddress)
+	o.logger.WithField("addr", o.options.SDPServer.ListenAddress).WithError(err).Error("SDP server ended with error")
 }
 
 func (o *WebRTCOutbound) Init() error {
